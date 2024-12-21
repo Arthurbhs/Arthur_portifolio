77e3e484fbe3b50d041721c18aa5d376574a4aa1
Arthur_bhs <arthur.shekinarfoxy@gmail.com>
2024-12-20 23:06:53 -0300

    atualização de link de projeto

--- src/pages/Home/sections/Projects/index.jsx
+++ src/pages/Home/sections/Projects/index.jsx
@@ -9,7 +9,7 @@ import primitive from "../../../../assets/images/primitive.png";
 import argosLogo from "../../../../assets/images/Argos_logo.png";
 
 const infoData = [
-  { imageSrc: argosHomeImage, title: "Argos", description: "web site ainda em desenvolvimento, para a recente empresa de seguros Argos", link: "https://argorseguros-prototype.netlify.app/" },
+  { imageSrc: argosHomeImage, title: "Argos", description: "web site ainda em desenvolvimento, para a recente empresa de seguros Argos", link: "https://argusseguros.com/" },
   { imageSrc: asseguraImage, title: "Assegura", description: "Web site para a franquia na baixada santista da empresa de clube beneficios e proteção veicular Assegura.", link: "https://assegura-prototype.netlify.app/" },
   { imageSrc: primitive, title: "Artes da Baixada pré-historica", description: "Ilustrações de logos feitas para uma Web serie criada por uma canal do yotube Primitive Domain", link: "https://www.instagram.com/p/DAe7drotn7L/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==" },
   { imageSrc: argosLogo, title: "Prototipos da logo Argos", description: "Todos os prototipos, ideias e rascunhos da logo criada para a empresa de seguros Argos", link: "https://www.instagram.com/p/DAe4k9NtHIr/?igsh=YzljYTk1ODg3Zg%3D%3D" },
